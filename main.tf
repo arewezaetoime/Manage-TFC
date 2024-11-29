4dff80d11da456d84a0569d5a7b1f2c3317bbe75
hristoiliev <hristoiliev.1994@gmail.com>
2024-11-29 22:26:46 +0200

    Including the full configuration files

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    tfe = {
+      version = "~> 0.60.1"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname     = var.default_hostname_hcp_terraform
+  token        = var.tfe_token
+  organization = var.organization_name
+}
+
+resource "tfe_project" "interview-task-project" {
+  organization = var.organization_name
+  name         = var.project_name
+}
+
+resource "tfe_oauth_client" "github_oauth_client" {
+  name             = var.github_oauth_client_name
+  organization     = var.organization_name
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.github_oauth_token_id
+  service_provider = "github"
+  organization_scoped = true
+}
+
+resource "tfe_workspace" "vcs-ws" {
+  name         = var.vcs_repository_name
+  organization = var.organization_name
+  project_id   = tfe_project.interview-task-project.id
+  queue_all_runs = true
+  vcs_repo {
+    branch         = var.branch_name
+    identifier     = var.repo_identifier
+    oauth_token_id = tfe_oauth_client.github_oauth_client.oauth_token_id
+  }
+}
+
+resource "tfe_workspace" "cli_workspaces" {
+  count        = 3
+  name         = "cli-workspace-${count.index + 1}"
+  organization = var.organization_name
+  project_id   = tfe_project.interview-task-project.id
+}
+
+resource "tfe_variable_set" "varset" {
+  name         = "Global Varset"
+  description  = "Variable set applied to all workspaces."
+  global       = false
+  organization = var.organization_name
+}
+
+resource "tfe_variable" "org_name_terraform_var" {
+  key             = "organization_name_hcp_variable"
+  value           = "The-Funky-Terraformers"
+  category        = "terraform"
+  description     = "This variable is containing the name of out organization"
+  variable_set_id = tfe_variable_set.varset.id
+}
+
+resource "tfe_variable" "org_name_env_var" {
+  key             = "organization_name_hcp_env_var"
+  value           = "The-Funky-Terraformers"
+  category        = "env"
+  description     = "an environment variable"
+  variable_set_id = tfe_variable_set.varset.id
+}
+
+data "tfe_variable_set" "varset_data" {
+  name         = tfe_variable_set.varset.name
+  organization = tfe_variable_set.varset.organization
+}
+
+resource "tfe_workspace_variable_set" "attach_varset_to_cli_workspaces" {
+  for_each = {
+    for i, workspace in tfe_workspace.cli_workspaces : workspace.name => workspace.id
+  }
+
+  workspace_id = each.value
+  variable_set_id = data.tfe_variable_set.varset_data.id
+}
---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "tfe_token" {
+  description = "Terraform Cloud API token. User/Team/Organization token"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_oauth_token_id" {
+  description = "Personal access token for connecting to Github VCS repository"
+  type = string
+  sensitive = true
+}
+
+variable "github_oauth_client_name" {
+  default = "my-github-oauth-client"
+  type = string
+}
+
+variable "organization_name" {
+  default = "The-Funky-Terraformers"
+  description = "HCP Organization name"
+  type        = string
+  sensitive   = false
+}
+
+variable "repo_identifier" {
+  default = "arewezaetoime/hcp-terraform"
+  description = "The path to the repository taken from the URI"
+  type = string
+}
+
+variable "default_hostname_hcp_terraform" {
+  default = "app.terraform.io"
+  description = "The default hostname for HCP Terraform"
+  type = string
+}
+
+variable "project_name" {
+  default = "interview-task-project"
+  description = "The name of the project"
+  type = string
+}
+
+variable "vcs_repository_name" {
+  default = "vcs-ws"
+  description = "The name of the VCS repository in workspace in HCP Terraform"
+  type = string
+}
+
+variable "branch_name" {
+  default = "main"
+  description = "The name of the prefered branch in the remote repository"
+  type = string
+}
